587344b31a0d8c979b4cf0a5b4f459204e94d632
Mehmetcan Yılmaz <ymehmetcan@hotmail.com>
2024-11-10 17:06:36 +0300

    feat: add stylelint-config

---
+++ packages/stylelint-config/src/rules/cssRules.ts
@@ -0,0 +1,13 @@
+export const cssRules = {
+  // General
+  'selector-max-combinators': 4,
+
+  // Color
+  'color-named': 'never',
+  'color-function-notation': 'legacy',
+  'alpha-value-notation': 'number',
+  'color-hex-length': null,
+
+  // Animation
+  'no-unknown-animations': true,
+};
